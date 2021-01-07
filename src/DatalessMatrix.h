c6287606b7865282854064b82004d0a3a26db222
CrazyWinner <mertcanozdemir23@gmail.com>
2021-01-07 19:18:44 +0300

    Bugfix

--- src/DatalessMatrix.h
+++ src/DatalessMatrix.h
@@ -1,13 +1,18 @@
 #pragma once
 #include "Matrix.h"
-struct DatalessMatrix : public LN::Matrix{
-   float (*atPtr)(uint16_t,uint16_t);
+struct DatalessMatrix : public LN::Matrix
+{
+    float (*atPtr)(uint16_t, uint16_t);
 
-   float at(uint16_t i, uint16_t j){
-    return atPtr(i,j);};
-   DatalessMatrix(float (*op)(uint16_t,uint16_t) ) : LN::Matrix(op){
-    atPtr = op;
+    float at(uint16_t i, uint16_t j)
+    {
+        return atPtr(i, j);
+    }
+    DatalessMatrix(float (*op)(uint16_t, uint16_t)) : LN::Matrix(op)
+    {
+        atPtr = op;
     };
-    ~DatalessMatrix(){
-      }
-  };
+    ~DatalessMatrix()
+    {
+    }
+};
